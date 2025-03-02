d8f241db189652783f31c6d83cb870cadaec7a4e
Azizbek <a298b112@gmail.com>
2025-03-02 12:35:51 +0100

    Implemented information stored in database feature, created successful message

--- UserApp/views.py
+++ UserApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .forms import ContactForm
+from .models import Form
+from django.contrib import messages
 
 
 def index(request):
@@ -10,4 +12,8 @@ def index(request):
             last_name = form.cleaned_data["last_name"]
             email = form.cleaned_data["email"]
             birth_date = form.cleaned_data["birth_date"]
+
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                                email=email, birth_date=birth_date)
+            messages.success(request, "Signed up successfully!")
     return render(request, "index.html")
