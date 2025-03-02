e7d9888bbe0c7c38bc4846a7e8c1dde9226edaec
Azizbek <a298b112@gmail.com>
2025-03-02 12:28:29 +0100

    Created a form model

--- UserApp/views.py
+++ UserApp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
 
 def index(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            birth_date = form.cleaned_data["birth_date"]
     return render(request, "index.html")
