7386fdbf9e7075276240cc1205051e3c8b304198
Azizbek <a298b112@gmail.com>
2025-03-21 18:05:42 +0100

    Deleted UserApp app, added main_page as default destination for the link

--- UserApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('sign-up', views.index, name='index'),
-    path('sign-in', views.sign_in, name='sign_in'),
-    path('main_page', views.home, name='main_page'),
-]
--- players/urls.py
+++ players/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('sign_up', views.players_signup, name='sign_up'),
     path('log_in', views.players_login, name='log_in'),
-    path('main_page', views.main_page, name='main_page'),
+    path('', views.main_page, name='main_page'),
 ]
