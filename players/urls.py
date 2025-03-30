8b557854320cfb5b554c7cf6134e40913a7b6813
Azizbek <a298b112@gmail.com>
2025-03-30 13:00:05 +0200

    Added logout/login/signup/home buttons, created and finished user profile page

--- players/urls.py
+++ players/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('sign_up', views.players_signup, name='sign_up'),
     path('log_in', views.players_login, name='log_in'),
     path('', views.main_page, name='main_page'),
+    path('log_out', views.players_logout, name='log_out'),
+    path('user_profile', views.players_profile, name='user_profile'),
 ]
--- players/views.py
+++ players/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.urls import reverse
 from .forms import CustomUserCreationForm
 
 
@@ -9,6 +10,7 @@ def players_signup(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect("log_in")
         else:
             form.add_error(None, "Invalid username or password.")
     else:
@@ -22,7 +24,8 @@ def players_login(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect("main_page")
+            return redirect("user_profile")
+
     else:
         form = AuthenticationForm()
     return render(request, "log_in.html", {"form": form})
@@ -30,3 +33,12 @@ def players_login(request):
 
 def main_page(request):
     return render(request, "main_page.html")
+
+
+def players_logout(request):
+    logout(request)
+    return redirect("main_page")
+
+
+def players_profile(request):
+    return render(request, "user_profile.html", {"user": request.user})
