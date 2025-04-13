7b818e1ee8f9848494c9ce4576237708b1db5dc3
Azizbek <a298b112@gmail.com>
2025-04-13 15:47:05 +0200

    Merge remote-tracking branch 'origin/templates' into templates
    # Conflicts:
    #	django_website/settings.py

--- players/views.py
+++ players/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.urls import reverse
 from .forms import CustomUserCreationForm
 from django.contrib import messages
 from django.http import HttpResponseForbidden
-from .forms import ProfileUpdateForm
 
 
 def players_signup(request):
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            form.save()
             return redirect("log_in")
+        else:
+            form.add_error(None, "Invalid username or password.")
     else:
         form = CustomUserCreationForm()
     return render(request, "sign_up.html", {"form": form})
@@ -41,17 +43,7 @@ def players_logout(request):
 
 
 def players_profile(request):
-    if request.method == 'POST':
-        form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-        if form.is_valid():
-            form.save()
-    else:
-        form = ProfileUpdateForm(instance=request.user.profile)
-
-    return render(request, "user_profile.html", {
-        "user": request.user,
-        "form": form,
-    })
+    return render(request, "user_profile.html", {"user": request.user})
 
 
 def about(request):
