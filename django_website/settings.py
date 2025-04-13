db9b6fe6c04cc875695284b464adefa9ff72578f
Azizbek <a298b112@gmail.com>
2025-04-13 16:09:22 +0200

    new profile_photo branch, removed app password

--- django_website/settings.py
+++ django_website/settings.py
@@ -132,9 +132,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'a298b112@gmail.com'
-EMAIL_HOST_PASSWORD = 'kiyn qvjo iixb dwgc'
-DEFAULT_FROM_EMAIL = 'a298b112@gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = ''
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
