89ba9aa70fb8df328638a483aa7c326ba596bd05
Azizbek <a298b112@gmail.com>
2025-04-22 18:57:24 +0200

    Finished writing forms.py, updated urls.py, added views in views.py, added blog links in navbar, created create_posts.html and all_posts.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
+
+app_name = "blog"
 
 urlpatterns = [
-
+    path("create_post/", views.create_posts_view, name="create_posts"),
+    path("posts_list/", views.all_posts_view, name="all_posts"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .forms import PostForm
 
-# Create your views here.
+
+def create_posts_view(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = PostForm()
+    return render(request, "create_posts.html", {"form": form})
+
+
+def all_posts_view(request):
+    return render(request, "all_posts.html")
