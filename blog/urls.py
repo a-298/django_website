79fcd4a9e87c24b90d428b8a6f9399408a7d3eb0
Azizbek <a298b112@gmail.com>
2025-04-23 19:53:36 +0200

    Finished writing latest_posts.html and create_posts.html, added specific_posts.html for showing full blog post, modified urls.py, views.py and base.html

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path("create_post/", views.create_posts_view, name="create_posts"),
-    path("posts_list/", views.all_posts_view, name="all_posts"),
+    path("create_posts/", views.create_posts_view, name="create_posts"),
+    path("latest_posts/", views.latest_posts_view, name="latest_posts"),
+    path("latest_posts/<int:post_id>/", views.specific_posts_view, name="specific_posts"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+from .models import Post
 
 
 def create_posts_view(request):
@@ -12,5 +13,11 @@ def create_posts_view(request):
     return render(request, "create_posts.html", {"form": form})
 
 
-def all_posts_view(request):
-    return render(request, "all_posts.html")
+def latest_posts_view(request):
+    posts = Post.objects.order_by("-date")[:5]
+    return render(request, "latest_posts.html", {"posts": posts})
+
+
+def specific_posts_view(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, "specific_posts.html", {"post": post})
