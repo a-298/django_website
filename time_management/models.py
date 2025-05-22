300c2c3587db2d12c1fb0193aa4dbea4305d1c1a
Azizbek <a298b112@gmail.com>
2025-05-22 20:40:32 +0200

    Finished writing initial page of TimeManagement app. DW-36

--- time_management/models.py
+++ time_management/models.py
@@ -1,9 +1,13 @@
+from django.utils import timezone
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Time(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(default=timezone.now)
     console_time = models.IntegerField()
-    pc_time = models.IntegerField()
+    computer_time = models.IntegerField()
     mobile_time = models.IntegerField()
     television_time = models.IntegerField()
 
--- time_management/views.py
+++ time_management/views.py
@@ -7,8 +7,11 @@ def home(request):
     if request.method == "POST":
         form = TimeForm(request.POST)
         if form.is_valid():
-            post = form.save()
+            entry = form.save(commit=False)
+            entry.user = request.user
+            entry.save()
             messages.success(request, "Your data was successfully recorded.")
     else:
         form = TimeForm()
     return render(request, "home.html", {"form": form})
+
