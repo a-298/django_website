9954062617505c284e75a179b94f9a31af79a9a1
Azizbek <a298b112@gmail.com>
2025-05-31 11:51:55 +0200

    Set 0 as default value in form. DW-44

--- time_management/forms.py
+++ time_management/forms.py
@@ -6,3 +6,11 @@ class TimeForm(forms.ModelForm):
     class Meta:
         model = Time
         fields = ["console_time", "computer_time", "mobile_time", "television_time"]
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.initial["console_time"] = 0
+        self.initial["computer_time"] = 0
+        self.initial["mobile_time"] = 0
+        self.initial["television_time"] = 0
